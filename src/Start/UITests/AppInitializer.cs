0a51f4f9eaf10e6921ec64d2416b70cb734e7942
IWASE Shigeaki <siwa32@gmail.com>
2018-08-11 23:38:48 +0900

    UITest

--- src/Start/UITests/AppInitializer.cs
+++ src/Start/UITests/AppInitializer.cs
@@ -1,1 +1,27 @@
-﻿ 
+﻿using Xamarin.UITest;
+
+namespace XamAppCenterSample2018.UITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .EnableLocalScreenshots()
+                    .PreferIdeSettings()
+                    .InstalledApp("com.siwa32.XamAppCenterSample2018")
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                .EnableLocalScreenshots()
+                .PreferIdeSettings()
+                .InstalledApp("com.siwa32.XamAppCenterSample2018")
+                .StartApp();
+        }
+    }
+}
